dca7a4d66fe35564bd026051a78ca59c20238bf9
Nhan Nguyen <ducnhan813@gmail.com>
2013-04-14 13:53:45 +0700

    Install some more packages

--- scripts/apply-myubuntu.sh
+++ scripts/apply-myubuntu.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Install some basic packages
+#packages="git git-core vim build-essential rake"
+#echo deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib | sudo tee /etc/apt/sources.list.d/virtualbox.list
+#wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O - | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install -y $packages
+
+# Install janus vim
+#curl -Lo- http://bit.ly/janus-bootstrap | bash
+
 terminal_dotfiles=(".bashrc" ".bash_aliases" ".vimrc.after")
 linked_directories=("scripts" ".janus" ".vim/colors")
 
@@ -33,3 +44,7 @@ ln -s ~/myubuntu/terminal/vim/colors ~/.vim/colors
 ln -s ~/myubuntu/terminal/janus ~/.janus
 
 source .bashrc
+
+# Install vagrant
+#sudo apt-get install linux-headers-$(uname -r) dkms virtualbox-4.2 rubygems ruby-dev
+#sudo gem install vagrant
