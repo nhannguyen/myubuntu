cddbc1d6e28045c0a28e9f159cf01cc10a461b5d
Nhan Nguyen <ducnhan813@gmail.com>
2013-04-14 22:19:48 +0700

    Tweaks

--- scripts/apply-myubuntu.sh
+++ scripts/apply-myubuntu.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 # Install some basic packages
-#packages="git git-core vim build-essential rake"
-#echo deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib | sudo tee /etc/apt/sources.list.d/virtualbox.list
-#wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O - | sudo apt-key add -
-#sudo apt-get update
-#sudo apt-get install -y $packages
+packages="git git-core vim build-essential rake"
+
+# Add virtual box repo if not added
+if [ ! -f /etc/apt/source.list.d/virtualbox.list ]
+then
+  echo deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib | tee /etc/apt/sources.list.d/virtualbox.list
+  wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O - | apt-key add -
+fi
+apt-get update
+apt-get install -y $packages
 
 # Install janus vim
-#curl -Lo- http://bit.ly/janus-bootstrap | bash
+if [ ! -f ~/.vim/installed ]
+then
+  sudo -u $USER curl -Lo- http://bit.ly/janus-bootstrap | bash
+  sudo -u $USER touch .vim/installed
+fi
+
+# Install vagrant
+apt-get install linux-headers-$(uname -r) dkms virtualbox-4.2 rubygems ruby-dev
+gem install vagrant
 
 terminal_dotfiles=(".bashrc" ".bash_aliases" ".vimrc.after")
 linked_directories=("scripts" ".janus" ".vim/colors")
 
-cd ~
+cd /home/$USER
 for dotfile in "${terminal_dotfiles[@]}"
 do
   if [ -h $dotfile ]
@@ -36,15 +49,11 @@ do
   fi
 done
 
-ln -s ~/myubuntu/terminal/_bash_aliases ~/.bash_aliases
-ln -s ~/myubuntu/scripts ~/scripts
-ln -s ~/myubuntu/terminal/_bashrc ~/.bashrc
-ln -s ~/myubuntu/terminal/_vimrc.after ~/.vimrc.after
-ln -s ~/myubuntu/terminal/vim/colors ~/.vim/colors
-ln -s ~/myubuntu/terminal/janus ~/.janus
+sudo -u $USER ln -s ~/myubuntu/terminal/_bash_aliases ~/.bash_aliases
+sudo -u $USER ln -s ~/myubuntu/scripts ~/scripts
+sudo -u $USER ln -s ~/myubuntu/terminal/_bashrc ~/.bashrc
+sudo -u $USER ln -s ~/myubuntu/terminal/_vimrc.after ~/.vimrc.after
+sudo -u $USER ln -s ~/myubuntu/terminal/vim/colors ~/.vim/colors
+sudo -u $USER ln -s ~/myubuntu/terminal/janus ~/.janus
 
-source .bashrc
-
-# Install vagrant
-#sudo apt-get install linux-headers-$(uname -r) dkms virtualbox-4.2 rubygems ruby-dev
-#sudo gem install vagrant
+sudo -u $USER source .bashrc
