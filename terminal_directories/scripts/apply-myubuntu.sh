c7ff2e1f2e3d46eaee7a6932031f7d1493a10c68
Nhan Nguyen <ducnhan813@gmail.com>
2013-04-17 15:00:54 +0700

    Alias for tmux and split installation of dotfiles

--- terminal_directories/scripts/apply-myubuntu.sh
+++ terminal_directories/scripts/apply-myubuntu.sh
@@ -23,30 +23,4 @@ fi
 apt-get install -y linux-headers-$(uname -r) dkms virtualbox-4.2 rubygems ruby-dev
 gem install vagrant --no-rdoc --no-ri
 
-terminal_dotfiles=("bashrc" "bash_aliases" "vimrc.after" "gitconfig" "tmux.conf")
-terminal_directories=("scripts" "janus" "vim/colors")
-
-cd /home/$SUDO_USER
-for dotfile in "${terminal_dotfiles[@]}"
-do
-  if [ -h .$dotfile ]
-  then
-    rm .$dotfile
-  elif [ -f .$dotfile ]
-  then
-    mv .$dotfile .$dotfile.old
-  fi
-  sudo -u $SUDO_USER ln -s /home/$SUDO_USER/myubuntu/terminal_dotfiles/_$dotfile /home/$SUDO_USER/.$dotfile
-done
-
-for terminal_directory in "${terminal_directories[@]}"
-do
-  if [ -h .$terminal_directory ]
-  then
-    rm .$terminal_directory
-  elif [ -d .$terminal_directory ]
-  then
-    mv .$terminal_directory .$terminal_directory.old
-  fi
-  sudo -u $SUDO_USER ln -s /home/$SUDO_USER/myubuntu/terminal_directories/$terminal_directory .$terminal_directory
-done
+myubuntu/terminal_directories/scripts/apply-terminal.sh
