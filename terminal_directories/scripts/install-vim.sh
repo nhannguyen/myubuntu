844db5474d21b281a55bf41893fea01a5bf243a5
Nhan Nguyen <ducnhan813@gmail.com>
2013-04-26 23:48:29 +0700

    Need rake too

--- terminal_directories/scripts/install-vim.sh
+++ terminal_directories/scripts/install-vim.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /home/$SUDO_USER/.vim/installed ]
 then
-  apt-get install -y curl
+  apt-get install -y curl rake
   sudo -u $SUDO_USER curl -Lo- http://bit.ly/janus-bootstrap | sudo -u $SUDO_USER bash
   sudo -u $SUDO_USER touch .vim/installed
 fi
