63e6be11e4a9e7fd60a269ba2ff861af29bf96ea
Nhan Nguyen <ducnhan813@gmail.com>
2013-05-09 10:23:05 +0700

    Enable selective installation

--- terminal_directories/scripts/apply-myubuntu.sh
+++ terminal_directories/scripts/apply-myubuntu.sh
@@ -2,6 +2,36 @@
 
 set -e
 
+vim=0
+vagrant=0
+
+if [[ $# -gt 2 ]]
+then
+  echo "Unsupported"
+  return 1
+elif [[ $# -eq 0 ]]
+then
+  vim=1
+  vagrant=1
+else
+  if [[ $1 == $2 ]]
+  then
+    echo "Please select different componets to install"
+    return 1
+  else
+    if [[ $1 == "vim" || $2 == "vim" ]]
+    then
+      vim=1
+    elif [[ $1 == "vagrant" || $2 == "vagrant" ]]
+    then
+      vagrant=1
+    else
+      echo "Currently support vim and vagrant only"
+      return 1
+    fi
+  fi
+fi
+
 # Install some basic packages
 packages="git git-core vim build-essential rake curl tmux"
 
@@ -11,6 +41,14 @@ apt-get install -y $packages
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 target=$(readlink -f "$DIR")
 
-$target/install-vim.sh
-$target/install-vagrant.sh
+if [[ $vim -eq 1 ]]
+then
+  $target/install-vim.sh
+fi
+
+if [[ $vagrant -eq 1 ]]
+then
+  $target/install-vagrant.sh
+fi
+
 $target/apply-terminal.sh
